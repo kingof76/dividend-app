35434c49f661f56a56e1fb41d51734f968be3999
kingof76 <kingof76kr@gmail.com>
2026-09-06 04:12:52 +0900

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.dividend.calendar',
-  appName: '배당금캘린더',
-  webDir: 'www'
-};
-
-export default config;
